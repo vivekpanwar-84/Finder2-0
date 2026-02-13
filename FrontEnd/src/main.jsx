3eb4fdc6d8bccec8c141a3f85e85f8dd10fd3bfb
valentino84 <vivekpanwar84500@gmail.com>
2026-02-13 15:20:50 +0530

    new update

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter, } from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
