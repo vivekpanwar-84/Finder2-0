c640320d57b34841960e5e5b56e54cf37e13f37b
valentino84 <vivekpanwar84500@gmail.com>
2026-02-13 14:05:22 +0530

    new update

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -2,13 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-      <BrowserRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+  <BrowserRouter>
+    <ThemeProvider>
+
+      <App />
+
+    </ThemeProvider>
+  </BrowserRouter>
 )
